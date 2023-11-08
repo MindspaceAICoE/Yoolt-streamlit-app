d12ce1a2b1ce27b432b3194bd4b0f2b7134b3f7d
simplysom <48379601+simplysom@users.noreply.github.com>
2023-11-08 10:43:49 -0800

    made change to video-rendering

--- services/video_rendering_service.py
+++ services/video_rendering_service.py
@@ -1,41 +1,44 @@
 import tempfile
-from pprint import pprint
-
 import whisper
 from pytube import YouTube
-
-
-import tempfile
-from pprint import pprint
-import whisper
-from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 
 def transcribeVideoOrchestrator(youtube_url: str, model_name: str):
     with tempfile.TemporaryDirectory() as temp_dir:
-        video = downloadYoutubeVideo(youtube_url, temp_dir)
-        transcription = transcribe(video, model_name)
-        return transcription
+        try:
+            video = downloadYoutubeVideo(youtube_url, temp_dir)
+            if not video:
+                return "Video could not be downloaded."
+            transcription = transcribe(video, model_name)
+            return transcription
+        except Exception as e:
+            return f"An error occurred: {str(e)}"
 
 def transcribe(video: dict, model_name="medium"):
-    print("Transcribing...", video['name'])
-    print("Using model:", model_name)
-    model = whisper.load_model(model_name)
-    result = model.transcribe(video['path'])
-    pprint(result)
-    return result["text"]
+    try:
+        print(f"Transcribing: {video['name']}")
+        model = whisper.load_model(model_name)
+        result = model.transcribe(video['path'])
+        return result["text"]
+    except FileNotFoundError:
+        return "Transcription file not found."
+    except Exception as e:
+        return f"An error occurred during transcription: {str(e)}"
 
 def downloadYoutubeVideo(youtube_url: str, directory: str) -> dict:
-    print("Processing : " + youtube_url)
-    yt = YouTube(youtube_url)
-    file_path = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-        'resolution').desc().first().download(directory)
-    print("VIDEO NAME " + yt.title)
-    print("Download complete:" + file_path)
-    return {"name": yt.title, "thumbnail": yt.thumbnail_url, "path": file_path}
-
-def on_progress(stream, chunk, bytes_remaining):
-    """Callback function"""
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    pct_completed = bytes_downloaded / total_size * 100
-    print(f"Status: {round(pct_completed, 2)} %")
+    try:
+        print(f"Processing: {youtube_url}")
+        yt = YouTube(youtube_url)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        if stream:
+            file_path = stream.download(output_path=directory)
+            print(f"Download complete: {file_path}")
+            return {"name": yt.title, "thumbnail": yt.thumbnail_url, "path": file_path}
+        else:
+            return None
+    except VideoUnavailable:
+        print("Video is unavailable.")
+        return None
+    except Exception as e:
+        print(f"An error occurred during video download: {str(e)}")
+        return None
