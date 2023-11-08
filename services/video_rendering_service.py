a3684eb5814e7f09940e2bf20b77f1c041a1d5e1
simplysom <48379601+simplysom@users.noreply.github.com>
2023-11-08 10:36:33 -0800

    made change to video-rendering

--- services/video_rendering_service.py
+++ services/video_rendering_service.py
@@ -5,31 +5,33 @@ import whisper
 from pytube import YouTube
 
 
-def transcribeVideoOrchestrator(youtube_url: str,  model_name: str):
-    video = downloadYoutubeVideo(youtube_url)
-    transcription = transcribe(video, model_name)
-    return transcription
+import tempfile
+from pprint import pprint
+import whisper
+from pytube import YouTube
 
+def transcribeVideoOrchestrator(youtube_url: str, model_name: str):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        video = downloadYoutubeVideo(youtube_url, temp_dir)
+        transcription = transcribe(video, model_name)
+        return transcription
 
 def transcribe(video: dict, model_name="medium"):
     print("Transcribing...", video['name'])
     print("Using model:", model_name)
     model = whisper.load_model(model_name)
-    result = model.transcribe(video['path'], )
+    result = model.transcribe(video['path'])
     pprint(result)
     return result["text"]
 
-
-def downloadYoutubeVideo(youtube_url: str) -> dict:
+def downloadYoutubeVideo(youtube_url: str, directory: str) -> dict:
     print("Processing : " + youtube_url)
     yt = YouTube(youtube_url)
-    directory = tempfile.gettempdir()
     file_path = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
         'resolution').desc().first().download(directory)
-    print("VIDEO NAME " + yt._title)
+    print("VIDEO NAME " + yt.title)
     print("Download complete:" + file_path)
-    return {"name": yt._title, "thumbnail": yt.thumbnail_url, "path": file_path}
-
+    return {"name": yt.title, "thumbnail": yt.thumbnail_url, "path": file_path}
 
 def on_progress(stream, chunk, bytes_remaining):
     """Callback function"""
